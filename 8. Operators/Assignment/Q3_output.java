56a5740f1f87f58b6982b385417b7755dad9a865
pankajsharma81 <pankajsharma88279@gamil.com>
2024-08-06 12:52:09 +0530

    Done Operators Assignment

---
+++ 8. Operators/Assignment/Q3_output.java
@@ -0,0 +1,10 @@
+public class Q3_output {
+  public static void main(String[] args) {
+    int x, y, z;
+    x = y = z = 2;
+    x += y;
+    y -= z;
+    z /= (x + y);
+    System.out.println(x + " " + y + " " + z);
+  }
+}

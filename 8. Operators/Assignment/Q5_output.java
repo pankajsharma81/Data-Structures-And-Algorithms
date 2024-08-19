4ea3d65f596077402079ce45963d464e3d92d1b9
pankajsharma81 <pankajsharma88279@gamil.com>
2024-08-19 09:27:14 +0530

    Done Operators Assignment

---
+++ 8. Operators/Assignment/Q5_output.java
@@ -0,0 +1,9 @@
+public class Q5_output {
+  public static void main(String[] args) {
+    int x = 10, y = 5;
+    int exp1 = (y * (x / y + x / y));
+    int exp2 = (y * x / y + y * x / y);
+    System.out.println(exp1);
+    System.out.println(exp2);
+  }
+}

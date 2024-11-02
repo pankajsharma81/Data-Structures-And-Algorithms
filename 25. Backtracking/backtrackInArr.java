af3e2dc9ab77160940fe757e67a9141df649063c
pankajsharma81 <pankajsharma88279@gmail.com>
2024-11-02 22:43:12 +0530

    Backtracking on Array

---
+++ 25. Backtracking/backtrackInArr.java
@@ -0,0 +1,26 @@
+public class backtrackInArr {
+
+  public static void changeArr(int arr[], int i, int val) {
+    if (i == arr.length) {
+      printArr(arr);
+      return;
+    }
+
+    arr[i] = val;
+    changeArr(arr, i + 1, val + 1);
+    arr[i] = arr[i] - 2;
+  }
+
+  public static void printArr(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    int arr[] = new int[5];
+    changeArr(arr, 0, 1);
+    printArr(arr);
+  }
+}

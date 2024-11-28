dcdc5e2c3c7ffc52464e8e0e2c190fce6257b6f8
pankajsharma81 <pankajsharma88279@gmail.com>
2024-11-28 23:56:23 +0530

    Introduction to Linked List

---
+++ 27. Linked Lists (Part 1)/LinkedList.java
@@ -0,0 +1,14 @@
+public class LinkedList{
+
+  public static class Node{
+    int data;
+    Node next;
+    public Node(int data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  public static void main(String[] args) {
+    
+  }
+}

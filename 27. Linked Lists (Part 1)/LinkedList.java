e481f65bf9288d3bbd6887927e237933ab8b394a
pankajsharma81 <pankajsharma88279@gmail.com>
2024-12-02 22:33:48 +0530

    Add Last in LL

--- 27. Linked Lists (Part 1)/LinkedList.java
+++ 27. Linked Lists (Part 1)/LinkedList.java
@@ -29,9 +29,24 @@ public class LinkedList {
     head = newNode;
   }
 
+  public void addLast(int data){
+    Node newNode = new Node(data);
+
+    if(head == null){
+      head = tail = newNode;
+      return;
+    }
+
+    tail.next = newNode;
+    tail = newNode;
+  }
+
   public static void main(String[] args) {
     LinkedList ll = new LinkedList();
     ll.addFirst(1);
     ll.addFirst(2);
+
+    ll.addLast(3);
+    ll.addLast(4);
   }
 }
